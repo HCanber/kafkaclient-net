8f46e334cd893b589b4176a06661de4600c8a1b7
HCanber <hakan_github@canberger.se>
2014-01-28 16:44:20 +0100

    Add Broker class

---
+++ src/KafkaClient.Tests/Api/BrokerTests.cs
@@ -0,0 +1,25 @@
+using KafkaClient.Api;
+using KafkaClient.IO;
+using Xunit;
+using Xunit.Should;
+
+namespace KafkaClient.Tests.Api
+{
+	public class BrokerTests
+	{
+		[Fact]
+		public void Given_a_response_deserializes_correctly()
+		{
+			var bytes = new byte[]
+			{
+				0x12, 0x34, 0x56, 0x78,                                  //NodeId
+				0x00, 0x04,(byte) 't',(byte) 'e',(byte) 's',(byte) 't',  //Host
+				0x00, 0x11, 0x22, 0x33,                                  //Port
+			};
+			var broker = Broker.Deserialize(new ReadBuffer(bytes));
+			broker.NodeId.ShouldBe(0x12345678);
+			broker.Host.ShouldBe("test");
+			broker.Port.ShouldBe(0x00112233);
+		}
+	}
+}
---
+++ src/KafkaClient/Api/Broker.cs
@@ -0,0 +1,41 @@
+using KafkaClient.IO;
+
+namespace KafkaClient.Api
+{
+	public class Broker
+	{
+		private readonly int _nodeId;
+		private readonly string _host;
+		private readonly int _port;
+
+		private Broker(int nodeId, string host, int port)
+		{
+			_nodeId = nodeId;
+			_host = host;
+			_port = port;
+		}
+
+		public int NodeId
+		{
+			get { return _nodeId; }
+		}
+
+		public string Host
+		{
+			get { return _host; }
+		}
+
+		public int Port
+		{
+			get { return _port; }
+		}
+
+		public static Broker Deserialize(IReadBuffer buffer)
+		{
+			var nodeId = buffer.ReadInt();
+			var host = buffer.ReadShortString();
+			var port = buffer.ReadInt();
+			return new Broker(nodeId,host,port);
+		}
+	}
+}
