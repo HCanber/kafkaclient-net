912305492ef177d840a979dad47de9f12c00278c
HCanber <hakan_github@canberger.se>
2014-01-28 16:28:29 +0100

    Simplify string tests in FetchRequestTests

--- src/KafkaClient.Tests/Api/FetchRequestTests.cs
+++ src/KafkaClient.Tests/Api/FetchRequestTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using KafkaClient.Api;
+using KafkaClient.Tests.TestHelpers;
 using KafkaClient.Utils;
 using Xunit;
 using Xunit.Should;
@@ -15,8 +16,8 @@ namespace KafkaClient.Tests.Api
 			var bytes = request.Write();
 
 			var expectedSize = 2 + 2 + 4 + (2 + 6) +    //ApiKey + ApiVersion + CorrelationId + String_ClientId
-			                   4 + 4 + 4 + 4 +          //ReplicaId + MaxWaitTime + MinBytes + ArraySize_Topics
-			                   (2 + 4) + 4 + 4 + 8 + 4; // String_Topic + ArraySize_Partitions + Partition + FetchOffset + MaxBytes
+												 4 + 4 + 4 + 4 +          //ReplicaId + MaxWaitTime + MinBytes + ArraySize_Topics
+												 (2 + 4) + 4 + 4 + 8 + 4; // String_Topic + ArraySize_Partitions + Partition + FetchOffset + MaxBytes
 
 			var expectedTotalMessageLength = expectedSize + 4;
 			bytes.ShouldHaveLength(expectedTotalMessageLength);
@@ -25,27 +26,19 @@ namespace KafkaClient.Tests.Api
 			bytes.GetShortFromBigEndianBytes(4).ShouldBe<short>(1);	//ApiKey=FetchRequest
 			bytes.GetShortFromBigEndianBytes(6).ShouldBe<short>(0);	//ApiVersion
 			bytes.GetIntFromBigEndianBytes(8).ShouldBe(789);	//CorrelationId
-			bytes.GetShortFromBigEndianBytes(12).ShouldBe<short>(6);	//ClientId string length "client"
-			bytes[14].ShouldBe((byte)'c');	//ClientId
-			bytes[14].ShouldBe((byte)'c');	//ClientId
-			bytes[15].ShouldBe((byte)'l');	//ClientId
-			bytes[16].ShouldBe((byte)'i');	//ClientId
-			bytes[17].ShouldBe((byte)'e');	//ClientId
-			bytes[18].ShouldBe((byte)'n');	//ClientId
-			bytes[19].ShouldBe((byte)'t');	//ClientId
+			bytes.GetShortFromBigEndianBytes(12).ShouldBe<short>((short)"client".Length);	//ClientId string length
+			bytes.ShouldBeString(14, "client");                       //ClientId
+
 			bytes.GetIntFromBigEndianBytes(20).ShouldBe(-1);	//ReplicaId=No node id
 			bytes.GetIntFromBigEndianBytes(24).ShouldBe(456);	//MaxWaitTime
 			bytes.GetIntFromBigEndianBytes(28).ShouldBe(123);	//MinBytes
 			bytes.GetIntFromBigEndianBytes(32).ShouldBe(1);	  //Array size for Topics
 			bytes.GetShortFromBigEndianBytes(36).ShouldBe<short>(4);	//Topic string length "test"
-			bytes[38].ShouldBe((byte)'t');	//Topic
-			bytes[39].ShouldBe((byte)'e');	//Topic
-			bytes[40].ShouldBe((byte)'s');	//Topic
-			bytes[41].ShouldBe((byte)'t');	//Topic
+			bytes.ShouldBeString(38, "test");                   //Topic
 			bytes.GetIntFromBigEndianBytes(42).ShouldBe(1);	    //Array size for Partitions
 			bytes.GetIntFromBigEndianBytes(46).ShouldBe(42);	  //Partition
 			bytes.GetLongFromBigEndianBytes(50).ShouldBe(4711); //Offset
 			bytes.GetIntFromBigEndianBytes(58).ShouldBe(100);	  //MaxBytes, FetchSize
-		} 
+		}
 	}
 }
